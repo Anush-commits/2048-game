093faeeb3e5e83e3f324d2423de9ee80856903f7
An992 <dreact7@gmail.com>
2021-01-21 17:52:20 +0400

    2048 game

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let rows = document.querySelectorAll('.row'), matrix = [];
+let rows = document.querySelectorAll('.row'), matrix = [], score = 0, clearall = document.querySelector('.cached');
 [...rows].map(e => {
   const clm = [];
   [...e.children].map((c, i) => clm.push(''))
@@ -7,11 +7,20 @@ let rows = document.querySelectorAll('.row'), matrix = [];
 })
 
 const fill = matrix => matrix.map((e, i) => e.map((k, t) => {
- if (k !== '') {
-   [...rows[i].children][t].innerHTML = `<div class='cell'>${k}</div>`
- } else {
-   [...rows[i].children][t].innerHTML = ``
- }
+  const colors = {
+    2: '#33A1C9', 4: '#9aa7c1', 8: '#e2a6a6', 16: '#c37b7b', 32: '#9aa7c1', 64: '#cebcb4',
+    128: '#854442', 256: '#CD9B9B', 512: '#D66F62', '1024': '#DB9370', '2048': '#8B8B7A'
+  }
+  if (k !== '') {
+    if (k >= 126) {
+      [...rows[i].children][t].innerHTML = `<div class='cell' style="background-color:${colors[k]}"><a class="CellNumber">${k}</a></div>`
+    }
+    else {
+      [...rows[i].children][t].innerHTML = `<div class='cell' style="background-color:${colors[k]}">${k}</div>`
+    }
+  } else {
+    [...rows[i].children][t].innerHTML = ``
+  }
   return [...rows]
 }))
 
@@ -59,8 +68,6 @@ function reverseArr(pos, arr) {
         }
 
       });
-      console.log(move);
-
     }
     return e;
   });
@@ -79,6 +86,7 @@ function sum(arr, pos) {
         if (q !== '' && q == e[r + 1]) {
           copy[k].splice(r, 2, 2 * q);
           copy[k].push('')
+          AddScore()
         }
       }
     })
@@ -88,10 +96,12 @@ function sum(arr, pos) {
         if (y !== '' && y == v[i + 1] && pos == 'up') {
           horizontal.splice(i, 2, 2 * y);
           horizontal.push('')
+          AddScore()
         }
         if (y !== '' && y == v[i - 1] && pos == 'down') {
-          horizontal.splice(i-1, 2, 2 * y);
+          horizontal.splice(i - 1, 2, 2 * y);
           horizontal.unshift('')
+          AddScore()
         }
       })
       horizontal.map((i, n) => copy[n][k] = i)
@@ -102,10 +112,21 @@ function sum(arr, pos) {
   })
 
   return copy
-
 }
 
 window.addEventListener('keydown', e => {
+  matrix.map(e => {
+    if (e.includes(2048) || e.every(k => k!== '')) {
+      let x = ShowModal(` Game is over and the score is ${score}`);
+      let restartBtn = document.querySelector('#restart');
+      restartBtn.addEventListener('click', () => { // starts new game
+        ShowModal().style.display = 'none'
+        clearMatrix() // makes new board
+        AddScore(true); // new score from zero
+      })
+
+    }
+  })
   if (e.key == 'ArrowDown') {
     matrix = reverseArr('down', matrix);
     sum(matrix, 'down')
@@ -114,7 +135,6 @@ window.addEventListener('keydown', e => {
   if (e.key == 'ArrowUp') {
     matrix = reverseArr('up', matrix);
     sum(matrix, 'up')
-
   }
   if (e.key == 'ArrowRight') {
     matrix = reverseArr('right', matrix);
@@ -128,3 +148,38 @@ window.addEventListener('keydown', e => {
   }
   fill(matrix)
 })
+
+//get and change score
+function AddScore(makeZero) {
+  score = score + 5;
+  const currentScore = document.getElementById('Current_score');
+  currentScore.innerHTML = score
+  if (makeZero == true) {
+    score = 0
+    currentScore.innerHTML = score
+  }
+}
+
+// clear matrix
+
+clearall.addEventListener('click', () => {
+  let x = alert('Do you want to exit?')
+  clearMatrix()
+})
+function clearMatrix() { // clears the mtrix and restarts 
+  matrix = matrix.map(e => e.map(k => k = ''))
+  randomTwoes(matrix, 2)
+  randomTwoes(matrix, 2)
+  fill(matrix)
+  return matrix;
+}
+
+function ShowModal(msg) {
+  const modal = document.querySelector('#modal'), modal_txt = document.querySelector('#modal_txt');
+  modal.style.display = 'block'
+  if (modal_txt.textContent !== '') {
+    modal_txt.innerHTML = ''
+  }
+  modal_txt.innerHTML += msg;
+  return modal;
+}
